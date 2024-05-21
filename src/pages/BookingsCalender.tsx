5ef7d94699ae76f0df0a2f4f726f1e8785f173b9
fili0727 <123998888+fili0727@users.noreply.github.com>
2024-05-21 14:51:23 +0200

    calender view done

---
+++ src/pages/BookingsCalender.tsx
@@ -0,0 +1,12 @@
+
+import BookingsCalenderView from "../components/BookingsCalender/BookingsCalenderView"
+
+export default function BookingsCalender() {
+
+  return (<>
+  <div>
+    <h1>Bookings Calender</h1>
+  <BookingsCalenderView />
+  </div>
+  </>)
+}
