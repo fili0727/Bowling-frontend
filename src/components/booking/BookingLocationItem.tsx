5b89b12e37670f7ead57ccda696007589f7bf3b7
TheDanishMexican <danieljchristjansen@gmail.com>
2024-05-26 16:30:19 +0200

    did test data

--- src/components/booking/BookingLocationItem.tsx
+++ src/components/booking/BookingLocationItem.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import BookingLocation from '../../interfaces/BookingLocation'
 import '../../styling/bookingLocation-item.css'
 
@@ -6,10 +7,52 @@ export default function BookingLocationItem({
 }: {
     bookingLocation: BookingLocation
 }) {
+    const [kidsLane, setKidsLane] = useState(false)
+    const [isBowling, setIsBowling] = useState(false)
+    const [isAirhockey, setIsAirhockey] = useState(false)
+    const [isDining, setIsDining] = useState(false)
+
+    useEffect(() => {
+        if (bookingLocation.name.includes('Lane')) {
+            setIsBowling(true)
+        } else if (bookingLocation.name.includes('Station')) {
+            setIsAirhockey(true)
+        } else if (bookingLocation.name.includes('Table')) {
+            setIsDining(true)
+        }
+
+        if (
+            bookingLocation.name === 'Lane 1' ||
+            bookingLocation.name === 'Lane 2' ||
+            bookingLocation.name === 'Lane 3' ||
+            bookingLocation.name === 'Lane 4'
+        ) {
+            setKidsLane(true)
+        } else {
+            setKidsLane(false)
+        }
+    }, [bookingLocation])
+
+    console.log(bookingLocation.name)
+
     return (
         <div className="bookingLocation-item-container">
             <p>{bookingLocation.name}</p>
-            <p>Max amount of people: {bookingLocation.capacity}</p>
+            {isBowling && <p>Capacity: {bookingLocation.capacity}</p>}
+            {isAirhockey && <p>Hockey sticks: {bookingLocation.capacity}</p>}
+            {isDining && <p>Seats: {bookingLocation.capacity}</p>}
+            {kidsLane && (
+                <p
+                    style={{
+                        color: 'blue',
+                        position: 'absolute',
+                        right: '39px',
+                        fontSize: '0.8rem',
+                    }}
+                >
+                    *Suited for children
+                </p>
+            )}
         </div>
     )
 }
